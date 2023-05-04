ca2d40dad6dddf204ad2ee64a9bcdfbb93f94c10
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-05-04 19:26:57 +0200

    end of the exercise

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+class Movie {
+    public $title;
+    public $director;
+    public $yearReleased;
+    public $genres;
+
+    public function __construct(string $title, string $director, int $yearReleased, array $genres) {
+        $this->title = $title;
+        $this->director = $director;
+        $this->yearReleased = $yearReleased;
+        $this->genres = $genres;
+    }
+
+}
+
+$movie1 = new Movie("I Simposon: Il Film", "Homer Simpson", 2012, ["Commedy", "Adventure"]);
+$movie2 = new Movie("I Goonies", "Chunk!", 1984, ["Commedy", "Thriller"]);
+$movie3 = new Movie("Lord of the Ring", "Peter Jackson", 2001, ["Action", "Adventure", "Fantasy"]);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.scss">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>Movie list</title>
+</head>
+<body>
+    <main>
+        <h1>List of Movies</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Director</th>
+                    <th>Year Released</th>
+                    <th>Genres</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+
+                    $movies = [$movie1, $movie2, $movie3];
+
+                    foreach ($movies as $movie) {
+                        echo "<tr>";
+                        echo "<td>" . $movie->title . "</td>";
+                        echo "<td>" . $movie->director . "</td>";
+                        echo "<td>" . $movie->yearReleased . "</td>";
+                        echo "<td>" . implode(", ", $movie->genres) . "</td>";
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    <main>
+</body>
+</html>
