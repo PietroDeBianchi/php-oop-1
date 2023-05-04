9bab634420320a81a5287986a6c5791f7b614092
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-05-04 19:42:41 +0200

    done! :)

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $movie3 = new Movie("Lord of the Ring", "Peter Jackson", 2001, ["Action", "Adven
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.scss">
+    <link rel="stylesheet" href="style/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
